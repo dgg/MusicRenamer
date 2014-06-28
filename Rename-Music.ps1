30f8819991ae99a10be55c0e5d82572fecd38082
Daniel Gonzalez Garcia <danielgonnet@gmail.com>
2014-06-28 13:23:38 +0200

    use taglib-sharp to extract name information

--- Rename-Music.ps1
+++ Rename-Music.ps1
@@ -7,11 +7,35 @@ Param(
 )
 
 function Main () {
+    $script_directory = Split-Path -parent $PSCommandPath   
+	$base_dir  = resolve-path $script_directory
+    $tag_lib = "$base_dir\taglib-sharp.dll"
+    [system.reflection.assembly]::loadfile($tag_lib)
+
     if ($file){
-        Write-host "rename single file $name"
+        # Write-host "rename single file $name"
+        $info = extract-tag-info $name
+        Write-Host $info
+        calculate-file-name $name
     }
     if ($directory){
         Write-host "rename all files from directory $path"
     }
 }
+
+function extract-tag-info([System.IO.FileInfo]$f){
+    $mp3 = [TagLib.File]::create($f.FullName);
+    $contributingInfo = @{
+        Artist = $mp3.Tag.FirstArtist
+        Title = $mp3.Tag.Title
+        TrackNumber = $mp3.Tag.Track
+    }
+    return New-Object psobject -Property $contributingInfo
+}
+
+function calculate-file-name($info, $includeSong)
+{
+    
+}
+
 Main
