b2de1cbcb36e6b022524c3938cd981385843ba08
Daniel Gonzalez Garcia <danielgonnet@gmail.com>
2014-06-28 12:00:05 +0200

    script skeleton and param handling
    two mutually exclusive ways of operating:
    * file: a single file will be renamed
    * directory: all files from directory will be renamed

---
+++ Rename-Music.ps1
@@ -0,0 +1,17 @@
+﻿[CmdletBinding()]
+Param(
+	[Parameter(ParameterSetName = "File", Mandatory = $true)][Switch][Alias('f')]$file,
+    [Parameter(ParameterSetName = "File", Mandatory = $true)][System.IO.FileInfo]$name,
+    [Parameter(ParameterSetName = "Directory", Mandatory=$true)][Switch][Alias('d')]$directory,
+    [Parameter(ParameterSetName = "Directory", Mandatory = $true)][System.IO.FileInfo]$path
+)
+
+function Main () {
+    if ($file){
+        Write-host "rename single file $name"
+    }
+    if ($directory){
+        Write-host "rename all files from directory $path"
+    }
+}
+Main
